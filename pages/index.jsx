2d28d54af1300203e9c768077945807608cf6dc4
Fincorelabs <fincorelabs@gmail.com>
2025-07-12 09:02:03 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,7 +36,7 @@ export default function Home() {
         </div>
 
         {/* Seta animada */}
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
+        <div className="absolute bottom-12 left-1/2 transform -translate-x-1/2 animate-bounce">
           <svg className="w-6 h-6 text-[#c7a254]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
           </svg>
